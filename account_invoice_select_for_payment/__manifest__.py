c6bd1599a85ce373504449fd85d7ce6daec701ee
Sebastian Ruiz <ingenieria@peti.com.co>
2024-09-30 13:18:15 -0500

    SR - Migracion account_invoice_select_for_payment

---
+++ account_invoice_select_for_payment/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Account Invoice Select for Payment",
+    "version": "17.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Camptocamp,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/bank-payment",
+    "category": "Banking addons",
+    "depends": ["account"],
+    "data": ["views/account_move.xml"],
+    "development_status": "Stable",
+    "installable": True,
+}
