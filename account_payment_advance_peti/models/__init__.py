4676960683f183f6e0ac2991d068372872679a68
PETISOLUCIONES <ingenieria@peti.com.co>
2024-11-21 16:00:07 -0500

    account_payment_advance_peti

--- account_payment_advance_peti/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import res_company
-from . import res_config
-from . import account_account
-from . import account_payment
-from . import account_move
--- account_payment_advance_peti/wizard/__init__.py
+++
@@ -1,1 +0,0 @@
-from . import account_advance_payment_invoice
