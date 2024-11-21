331e5341a16c450ffc507527532d262fef3716fb
PETISOLUCIONES <ingenieria@peti.com.co>
2024-11-21 16:54:53 -0500

    ok

--- bi_vendor_registration_request/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
-##############################################################################
-
-from . import vendor_request
-# from . import res_country
