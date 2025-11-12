5f00695f77ffe3d1cd5d89b87f24e347c46c75c5
Camilo <78372387+PETISOLUCIONES@users.noreply.github.com>
2025-11-12 08:38:55 -0500

    [FIX] corregir error de listar productos

--- sr_product_based_pricelist/models/sale.py
+++ sr_product_based_pricelist/models/sale.py
@@ -16,36 +16,51 @@ class ProductProduct(models.Model):
     _inherit = "product.product"
 
     @api.model
-    def _name_search(self, name, args=None, operator='ilike', order=None, limit=100, name_get_uid=None):
-        args = args or []
-        product_ids = []
-
+    def _name_search(self, name, domain=None, operator='ilike', limit=None, order=None):
+        domain = domain or []
         if name:
             positive_operators = ['=', 'ilike', '=ilike', 'like', '=like']
+            product_ids = []
             if operator in positive_operators:
-                product_ids = self._search([('default_code', '=', name)] + args, limit=limit, access_rights_uid=name_get_uid) or \
-                              self._search([('barcode', '=', name)] + args, limit=limit, access_rights_uid=name_get_uid)
-
-            if not product_ids:
-                product_ids = self._search(args + [('default_code', operator, name)], limit=limit) or \
-                              self._search(args + [('name', operator, name)], limit=limit)
-
-            if not product_ids:
-                ptrn = re.search(r'(\[(.*?)\])', name)
-                if ptrn:
-                    product_ids = self._search([('default_code', '=', ptrn.group(2))] + args, limit=limit,
-                                               access_rights_uid=name_get_uid)
-
+                product_ids = list(self._search([('default_code', '=', name)] + domain, limit=limit, order=order))
+                if not product_ids:
+                    product_ids = list(self._search([('barcode', '=', name)] + domain, limit=limit, order=order))
+            if not product_ids and operator not in expression.NEGATIVE_TERM_OPERATORS:
+                # Do not merge the 2 next lines into one single search, SQL search performance would be abysmal
+                # on a database with thousands of matching products, due to the huge merge+unique needed for the
+                # OR operator (and given the fact that the 'name' lookup results come from the ir.translation table
+                # Performing a quick memory merge of ids in Python will give much better performance
+                product_ids = list(self._search(domain + [('default_code', operator, name)], limit=limit, order=order))
+                if not limit or len(product_ids) < limit:
+                    # we may underrun the limit because of dupes in the results, that's fine
+                    limit2 = (limit - len(product_ids)) if limit else False
+                    product2_ids = self._search(domain + [('name', operator, name), ('id', 'not in', product_ids)],
+                                                limit=limit2, order=order)
+                    product_ids.extend(product2_ids)
+            elif not product_ids and operator in expression.NEGATIVE_TERM_OPERATORS:
+                domain2 = expression.OR([
+                    ['&', ('default_code', operator, name), ('name', operator, name)],
+                    ['&', ('default_code', '=', False), ('name', operator, name)],
+                ])
+                domain2 = expression.AND([domain, domain2])
+                product_ids = list(self._search(domain2, limit=limit, order=order))
+            if not product_ids and operator in positive_operators:
+                ptrn = re.compile(r'(\[(.*?)\])')
+                res = ptrn.search(name)
+                if res:
+                    product_ids = list(
+                        self._search([('default_code', '=', res.group(2))] + domain, limit=limit, order=order))
+            # still no results, partner in context: search on supplier info as last hope to find something
             if not product_ids and self._context.get('partner_id'):
                 suppliers_ids = self.env['product.supplierinfo']._search([
                     ('partner_id', '=', self._context.get('partner_id')),
-                    '|', ('product_code', operator, name), ('product_name', operator, name)],
-                    access_rights_uid=name_get_uid)
+                    '|',
+                    ('product_code', operator, name),
+                    ('product_name', operator, name)])
                 if suppliers_ids:
                     product_ids = self._search([('product_tmpl_id.seller_ids', 'in', suppliers_ids)], limit=limit,
-                                               access_rights_uid=name_get_uid)
-
-            if not product_ids and self._context.get('pricelist'):
+                                               order=order)
+            if not product_ids and self._context.get('partner_id'):
                 pricelist = self.env['product.pricelist'].browse(self._context.get('pricelist'))
                 product_list = []
                 for record in pricelist.item_ids:
@@ -54,14 +69,17 @@ class ProductProduct(models.Model):
                     elif record.applied_on == '3_global':
                         product_list.extend(self.env['product.product'].ids)
                     elif record.applied_on == '2_product_category':
-                        product_list.extend(self.env['product.product'].search([('categ_id', 'child_of', record.categ_id.id)]).ids)
+                        product_list.extend(
+                            self.env['product.product'].search([('categ_id', 'child_of', record.categ_id.id)]).ids)
                     elif record.applied_on == '1_product':
-                        product_list.extend(self.env['product.product'].search([('product_tmpl_id', '=', record.product_tmpl_id.id)]).ids)
+                        product_list.extend(self.env['product.product'].search(
+                            [('product_tmpl_id', '=', record.product_tmpl_id.id)]).ids)
 
                 if product_list:
                     domain = [('id', 'in', product_list)]
-                    product_ids = self._search(domain, limit=limit, access_rights_uid=name_get_uid)
-
+                    product_ids = self._search(domain, limit=limit)
+        else:
+            product_ids = self._search(domain, limit=limit, order=order)
         return product_ids
 
 
